c3d96584f93bf44f67ae86371fd2762af7d3e4be
cjchallis <christopher.challis@gmail.com>
2015-02-07 21:22:28 -0500

    Add Dirichlet probabilities to tourney results.
    Also shuffle player order with each game and fix chrisStrategies to
    handle change to bestFold().

--- tourney.py
+++ tourney.py
@@ -1,19 +1,52 @@
+try:
+    import numpy as np
+    numpy = True
+except ImportError:
+    print("The module numpy was not found.  Probabilities will not be reported in results.")
+
 import pairsClasses as p
 from strategies.chrisStrategies import FoldLowWithHigh
 from strategies.chrisStrategies import Expectation
+from random import shuffle
+
+strategies = [FoldLowWithHigh(3,1),
+              FoldLowWithHigh(4,8),
+              Expectation()]
+strat_names = ["Folder_3", "Folder_4_8", "Expectation"]
+n_strats = len(strategies)
+
+for i in range(n_strats):
+    strategies[i].tourney_index = i
+    strategies[i].tourney_name = strat_names[i]
 
 losers = []
-for i in range(50):
+for i in range(100):
     # defaults to 5 players that always hit
-    d = p.Dealer(4)
-    # replace some strategies
-    d.gameState.players[0].strategy = FoldLowWithHigh(3, 8)
-    d.gameState.players[1].strategy = FoldLowWithHigh(4, 8)
-    d.gameState.players[2].strategy = Expectation()
-    # play a game and append the loser index
-    losers.append(d.play())
+    d = p.Dealer(n_strats)
+    shuffle(strategies)
+    for i in range(n_strats):
+        d.gameState.players[i].strategy = strategies[i]
+    # play a game and get the index of the losing strategy
+    lost = d.gameState.players[d.play()].strategy.tourney_index
+    losers.append(lost)
 
-# changed this in anticipation index change from Alex
-# may need +1 to work correctly
-for i in range(len(d.gameState.players)):
-    print("Player " + str(i) + ": " + str(losers.count(i)))
+data = []
+for i in range(n_strats):
+    data.append(losers.count(i))
+
+print("Games Lost:")
+for i in range(n_strats):
+    print(strat_names[i] + ":\t" + str(data[i]))
+
+if numpy:
+    prior = np.repeat(10, n_strats)
+    posterior = prior + np.array(data)
+    N = 10000
+    draws = np.random.dirichlet(posterior, N)
+    best = np.bincount(np.argmin(draws, axis = 1), minlength = n_strats) / N
+    worst = np.bincount(np.argmax(draws, axis = 1), minlength = n_strats) / N
+
+    print("\n\n\t\tP(best)\tP(worst)")
+    for i in range(n_strats):
+        print(strat_names[i] + "\t" + '%.2f' % best[i] + "\t" + '%.2f' % worst[i])
+
