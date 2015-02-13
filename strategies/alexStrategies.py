3b8d4f12f6b55a1b59ad592d050330eb70cc2273
Alex Mooney <alexmmooney@gmail.com>
2015-02-13 11:51:44 -0500

    Add CruelFold

--- strategies/alexStrategies.py
+++ strategies/alexStrategies.py
@@ -30,3 +30,28 @@ class CardCounter:
             return 'Hit me'
         else:
             return 'fold'
+
+class CruelFold:
+    def __init__(self, scared=0.23, malice=0.5):
+        from collections import Counter
+        self.Counter = Counter
+        self.scared = scared
+        self.malice = malice
+
+    def play(self, info):
+        numPlayers = info.noPlayers
+        hitPoints = [(60/numPlayers) - p.getScore() for p in info.players]
+        folds = sorted(info.bestFolds(), key=lambda t: t[1])[:2] # best two cards
+        pkill = 0
+        c = self.Counter(info.deck)
+        for i, hp in enumerate(hitPoints):
+            if i == self.player.index():
+                continue
+            if hp >= folds[0][1] and hp < folds[1][1]:
+                pkill += sum([s*c[s]/len(info.deck) for s in info.players[i].stack])
+
+        if info.bestFold(self.player)[1] - self.malice*pkill > self.scared*sum([s*c[s] for s in c])/len(info.deck) + sum([s*c[s]/len(info.deck) for s in self.player.stack]):
+            return 'Hit me'
+        else:
+            return 'fold'
+
